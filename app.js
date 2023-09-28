4009eb68e09ec4b7cc92fcbe8c3fb0ea92ba5f71
xRezio <edwin.carin@epitech.eu>
2023-09-28 16:25:43 +0200

    almost finished

---
+++ app.js
@@ -0,0 +1,19 @@
+
+    const texteDynamique = document.getElementById('texte-dynamique');
+    const texteAInserer = "Bienvenue sur mon CV en ligne.";
+
+    // Fonction pour ajouter le texte progressivement
+    function afficherTexteProgressivement(texte, vitesse) {
+        let index = 0;
+        const interval = setInterval(() => {
+            texteDynamique.textContent += texte[index];
+            index++;
+            if (index === texte.length) {
+                clearInterval(interval); // Arrêter l'effet de frappe une fois terminé
+            }
+        }, vitesse);
+    }
+
+    // Démarrer l'effet de frappe
+    afficherTexteProgressivement(texteAInserer, 100); // 100 millisecondes de délai entre chaque caractère
+
